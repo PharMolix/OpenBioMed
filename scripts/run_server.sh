df9f8dbee94126cbd9b2b901080bef30c04cb2c9
youngking0727 <youngking0727@126.com>
2026-05-29 23:48:14 +0800

    [fix] Update run_server.sh port and GPU config

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,19 +3,19 @@
 echo "server start"
 
 # Use empty GPUs (GPU 0,1 have zombie processes)
-export CUDA_VISIBLE_DEVICES=2,3
+# export CUDA_VISIBLE_DEVICES=2,3
 
 # Activate the Conda environment
 source /opt/conda/bin/activate OpenBioMed
 
 python -m uvicorn open_biomed.scripts.run_server:app \
     --host 0.0.0.0 \
-    --port 32520 \
+    --port 32518 \
     --log-level info > ./tmp/server_test.log 2>&1 &
 
 python -m uvicorn open_biomed.scripts.run_server_workflow:app \
     --host 0.0.0.0 \
-    --port 32521 \
+    --port 32519 \
     --log-level info > ./tmp/workflow_test.log 2>&1 &
 
 tail -f /dev/null
